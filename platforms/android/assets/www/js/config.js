b28bc6b05cd73c07c92a278caba23ae7425a221b
Peter Korduan <peter.korduan@gdi-service.de>
2018-08-07 15:25:51 +0200

    Add fileOpener2 plugin.

--- platforms/android/assets/www/js/config.js
+++ platforms/android/assets/www/js/config.js
@@ -6,15 +6,12 @@ config = {
   startPosition: [12.20896, 53.95876],
   minZoom: 8,
   maxZoom: 18,
-  logLevel: 3, // 0 off, 1 error, 2 waring, 3 info, 4 debug
+  logLevel: 4, // 0 off, 1 error, 2 waring, 3 info, 4 debug
   debug: true,
   kvwmapServerId: 1,
   kvwmapServerName: 'kvwmap Server',
   kvwmapServerUrl: 'https://geoportal.lkros.de/kvwmap_dev',
   kvwmapServerUsername: 'pkorduan',
   kvwmapServerPasswort: '',
-  kvwmapServerStelleId: 610011,
-  backgroundLayerUrl: 'file:///storage/emulated/0/Android/data/de.gdiservice.kvmobile/cache/orka_lkros/'
-//  backgroundLayerUrl: 'https://www.orka-mv.de/geodienste/orkamv/tiles/1.0.0/orkamv/GLOBAL_WEBMERCATOR/',
-
+  kvwmapServerStelleId: 610011
 }
